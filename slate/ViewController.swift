a02a303827be241dd3ca27432f655676946b7634
Alex Macmillan <alex@x13n.com>
2015-08-29 17:00:12 -0400

    Outlet collection for function buttons

--- slate/ViewController.swift
+++ slate/ViewController.swift
@@ -9,18 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var powerButton: UIButton!
-    @IBOutlet var chromeButton: UIButton!
-    @IBOutlet var playstationButton: UIButton!
-    @IBOutlet var spotifyButton: UIButton!
-    
-    var buttons: Array<UIButton> = []
+    @IBOutlet var buttons: Array<UIButton>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        buttons = [powerButton, chromeButton, playstationButton, spotifyButton]
-        
         for button in buttons {
             button.addTarget(self, action: "buttonPressed:", forControlEvents: UIControlEvents.PrimaryActionTriggered)
         }
