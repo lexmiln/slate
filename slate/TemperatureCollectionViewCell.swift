bae87f1bd1dbf90f090d522e146ebbb589acafc3
Alex Macmillan <alex@x13n.com>
2015-08-29 16:59:48 -0400

    Images moved to Assets catalog

---
+++ slate/TemperatureCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TemperatureCollectionViewCell.swift
+//  slate
+//
+//  Created by Alex Macmillan on 29/08/2015.
+//  Copyright © 2015 x13N. All rights reserved.
+//
+
+import UIKit
+
+class TemperatureCollectionViewCell: UICollectionViewCell {
+    
+}
