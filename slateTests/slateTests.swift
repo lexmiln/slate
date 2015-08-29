e2f567dca2a6cfabc49139a83285aee8125f4631
Alex Macmillan <alex@x13n.com>
2015-08-29 10:46:49 -0400

    Initial Commit

---
+++ slateTests/slateTests.swift
@@ -0,0 +1,36 @@
+//
+//  slateTests.swift
+//  slateTests
+//
+//  Created by Alex Macmillan on 29/08/2015.
+//  Copyright © 2015 x13N. All rights reserved.
+//
+
+import XCTest
+@testable import slate
+
+class slateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
