88ef884dd299b3379356228d609b5b039f544f1e
Alex Macmillan <alex@x13n.com>
2015-08-29 15:51:55 -0400

    Pressable, selectable function buttons

--- slate/ViewController.swift
+++ slate/ViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var powerButton: UIButton!
+    @IBOutlet var chromeButton: UIButton!
+    @IBOutlet var playstationButton: UIButton!
+    @IBOutlet var spotifyButton: UIButton!
+    
+    var buttons: Array<UIButton> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        buttons = [powerButton, chromeButton, playstationButton, spotifyButton]
+        
+        for button in buttons {
+            button.addTarget(self, action: "buttonPressed:", forControlEvents: UIControlEvents.PrimaryActionTriggered)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func buttonPressed(sender: UIView) {
+        print("Button pressed")
+        for button in buttons {
+            button.selected = button == sender
+        }
     }
 
     func listen() {
